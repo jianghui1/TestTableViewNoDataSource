7d2e7e8f6041401762828ed940a8c1265a837951
jiangzenghui005@163.com <jiangzenghui005@163.com>
2019-01-22 09:55:21 +0800

    Initial Commit

---
+++ TestTableViewNoDataSource/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestTableViewNoDataSource
+//
+//  Created by ys on 2019/1/22.
+//  Copyright © 2019 mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestTableViewNoDataSource/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestTableViewNoDataSource
+//
+//  Created by ys on 2019/1/22.
+//  Copyright © 2019 mg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
